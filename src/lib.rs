f3eb4ebc1f2647d5d34c695055512b0bbb072b80
Øystein Hovind <hovindo@gmail.com>
2020-12-22 20:35:09 +0100

    Use auto trait feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 #![allow(incomplete_features)]
-#![feature(array_map, const_generics)]
+#![feature(array_map, auto_traits, const_generics, negative_impls)]
 
 #[cfg(test)]
 mod tests {
     use crate::*;
-
     #[test]
     fn it_works() {
         let u = Vector::<{Variance::Contra}, f64, 2>::zero();
@@ -12,13 +11,21 @@ mod tests {
         let q = u * v;
         println!("{:?}", q);
 
-        /*
-        let x = TwoForm::<f64, 10>::zero();
-        let y = Vector::<{Variance::Contra}, f64, 10>::zero();
-        let z = x * y * y;
+        let x = Matrix::<f64, 4, 2>::zero();
+        let y = Vector::<{Variance::Contra}, f64, 2>::zero();
+        let z : Vector<{Variance::Contra}, f64, 4> = x * y;
         println!("{:?}", z);
+
+        let a = Matrix::<f64, 2, 1>::zero();
+        let b = Matrix::<f64, 3, 2>::zero();
+        let c : Matrix<f64, 3, 1> = a * b;
+        println!("{:?}", c);
+
+        let r = TwoForm::<f64, 10>::zero();
+        let s = Vector::<{Variance::Contra}, f64, 10>::zero();
+        let t : f64 = r * s * s;
+        println!("{:?}", t);
         assert_eq!(2 + 2, 4);
-        */
     }
 }
 
@@ -30,8 +37,12 @@ pub enum Variance {
     Contra,
 }
 
+pub auto trait Distributive {}
+
 #[derive(Clone, Copy, Debug)]
 pub struct Vector<const V: Variance, T, const N: usize>([T; N]);
+
+impl<const V: Variance, T, const N: usize> !Distributive for Vector<V, T, N> {}
 
 
 pub trait Zero {
@@ -67,11 +78,9 @@ Self: Zero,
     }
 }
 
-
-
 impl<const V: Variance, S, T, U, const N: usize> Mul<T> for Vector<V, S, N> where
-T: Clone + Mul<S, Output = U>,
-S: Clone,
+S: Clone + Mul<T, Output = U>,
+T: Clone + Distributive,
 Vector<V, U, N>: Zero,
 {
     type Output = Vector<V, U, N>;
@@ -79,13 +88,38 @@ Vector<V, U, N>: Zero,
     fn mul(self, rhs: T) -> Self::Output {
         let mut x = Vector::<V, U, N>::zero();
         for i in 1..N {
-            x.0[i] = rhs.clone() * self.0[i].clone();
+            x.0[i] = self.0[i].clone() * rhs.clone();
         }
         x
     }
 }
 
-/*impl<const N: usize, S, T, U> Mul<Vector<{Variance::Contra}, T, N>> for Vector<{Variance::Co}, S, N> where
+impl<const V: Variance, const N: usize> Mul<Vector<V, f64, N>> for f64 where
+{
+    type Output = Vector<V, f64, N>;
+
+    fn mul(self, rhs: Vector<V, f64, N>) -> Self::Output {
+        Vector(rhs.0.map(|s| s * self.clone()))
+    }
+}
+
+impl<const V: Variance, S, T, U, const M: usize, const N: usize> Mul<Vector<V, T, M>> for Vector<V, S, N> where
+S: Clone + Mul<Vector<V, T, M>, Output = U>,
+Vector<V, T, M>: Clone,
+Vector<V, U, N>: Zero,
+{
+    type Output = Vector<V, U, N>;
+
+    fn mul(self, rhs: Vector<V, T, M>) -> Self::Output {
+        let mut x = Vector::<V, U, N>::zero();
+        for i in 1..N {
+            x.0[i] = self.0[i].clone() * rhs.clone();
+        }
+        x
+    }
+}
+
+impl<const N: usize, S, T, U> Mul<Vector<{Variance::Contra}, T, N>> for Vector<{Variance::Co}, S, N> where
 S: Clone + Mul<T, Output = U>,
 T: Clone,
 U: Add<U, Output = U> + Zero,
@@ -99,8 +133,24 @@ U: Add<U, Output = U> + Zero,
         }
         x
     }
-}*/
+}
 
-pub type Matrix<T, const N: usize, const M: usize> = Vector<{Variance::Co}, Vector<{Variance::Contra}, T, M>, N>;
+impl<const N: usize, S, T, U> Mul<Vector<{Variance::Co}, T, N>> for Vector<{Variance::Contra}, S, N> where
+S: Clone + Mul<T, Output = U>,
+T: Clone,
+U: Add<U, Output = U> + Zero,
+{
+    type Output = U;
+
+    fn mul(self, rhs: Vector<{Variance::Co}, T, N>) -> Self::Output {
+        let mut x = U::zero();
+        for i in 0..N {
+            x = x + self.0[i].clone() * rhs.0[i].clone();
+        }
+        x
+    }
+}
+
+pub type Matrix<T, const N: usize, const M: usize> = Vector<{Variance::Co}, Vector<{Variance::Contra}, T, N>, M>;
 
 pub type TwoForm<T, const N: usize> = Vector<{Variance::Co}, Vector<{Variance::Co}, T, N>, N>;
