d297ee87c876a50e309f47c3b06c725438f52d98
Øystein Hovind <oystein@ascenium.com>
2025-06-22 20:48:16 +0200

    Add more macro things

--- src/vector.rs
+++ src/vector.rs
@@ -58,21 +58,27 @@ Vector<V, U, N>: Zero,
     }
 }
 
-impl<const V: Variance, S, T, U, const M: usize, const N: usize> Mul<&Vector<V, S, M>> for &Vector<V, T, N> where
-for<'a, 'b> &'a T: Mul<&'b Vector<V, S, M>, Output = U>,
-Vector<V, U, N>: Zero,
-{
-    type Output = Vector<V, U, N>;
+macro_rules! impl_outer_product_mul {
+    ($lhs:ty, &$rhs:ty) => {
+        impl<const V: Variance, S, T, U, const M: usize, const N: usize> Mul<&$rhs> for $lhs where
+        for<'a, 'b> &'a T: Mul<&'b $rhs, Output = U>,
+        Vector<V, U, N>: Zero,
+        {
+            type Output = Vector<V, U, N>;
 
-    fn mul(self, rhs: &Vector<V, S, M>) -> Self::Output {
-        let mut x = Vector::<V, U, N>::zero();
-        for i in 0..N {
-            x.0[i] = &self.0[i] * rhs;
+            fn mul(self, rhs: &$rhs) -> Self::Output {
+                let mut x = Vector::<V, U, N>::zero();
+                for i in 0..N {
+                    x.0[i] = &self.0[i] * rhs;
+                }
+                x
+            }
         }
-        x
     }
 }
 
+impl_outer_product_mul!(&Vector<V, T, N>, &Vector<V, S, M>);
+impl_outer_product_mul!(Vector<V, T, N>, &Vector<V, S, M>);
 
 macro_rules! impl_inner_product_mul {
     ($lhs:ty, $rhs:ty) => {
@@ -90,7 +96,6 @@ macro_rules! impl_inner_product_mul {
                 x
             }
         }
-
     }
 }
 
@@ -99,32 +104,39 @@ impl_inner_product_mul!(Vector<{Variance::Contra}, T, N>, &Vector<{Variance::Co}
 impl_inner_product_mul!(&Vector<{Variance::Co}, T, N>, &Vector<{Variance::Contra}, S, N>);
 impl_inner_product_mul!(Vector<{Variance::Co}, T, N>, &Vector<{Variance::Contra}, S, N>);
 
-impl<const V: Variance, T, U, const N: usize> Mul<&f64> for &Vector<V, T, N> where
-for<'a, 'b> &'a T: Mul<&'b f64, Output = U>,
-Vector<V, U, N>: Zero,
-{
-    type Output = Vector<V, U, N>;
+macro_rules! impl_primitive_product_mul {
+    ($vec:ty, $primitive:ty) => {
+        impl<const V: Variance, T, U, const N: usize> Mul<&$primitive> for $vec where
+        for<'a, 'b> &'a T: Mul<&'b $primitive, Output = U>,
+        Vector<V, U, N>: Zero,
+        {
+            type Output = Vector<V, U, N>;
 
-    fn mul(self, rhs: &f64) -> Self::Output {
-        let mut x = Vector::<V, U, N>::zero();
-        for i in 0..N {
-            x.0[i] = &self.0[i] * &rhs;
+            fn mul(self, rhs: &$primitive) -> Self::Output {
+                let mut x = Vector::<V, U, N>::zero();
+                for i in 0..N {
+                    x.0[i] = &self.0[i] * &rhs;
+                }
+                x
+            }
         }
-        x
+
+        impl<const V: Variance, T, U, const N: usize> Mul<$vec> for &$primitive where
+        for<'a, 'b> &'a $primitive: Mul<&'b T, Output = U>,
+        Vector<V, U, N>: Zero,
+        {
+            type Output = Vector<V, U, N>;
+
+            fn mul(self, rhs: $vec) -> Self::Output {
+                let mut x = Vector::<V, U, N>::zero();
+                for i in 0..N {
+                    x.0[i] = self * &rhs.0[i];
+                }
+                x
+            }
+        }
     }
 }
 
-impl<const V: Variance, T, U, const N: usize> Mul<&Vector<V, T, N>> for &f64 where
-for<'a, 'b> &'a f64: Mul<&'b T, Output = U>,
-Vector<V, U, N>: Zero,
-{
-    type Output = Vector<V, U, N>;
-
-    fn mul(self, rhs: &Vector<V, T, N>) -> Self::Output {
-        let mut x = Vector::<V, U, N>::zero();
-        for i in 0..N {
-            x.0[i] = self * &rhs.0[i];
-        }
-        x
-    }
-}
+impl_primitive_product_mul!(&Vector<V, T, N>, f64);
+impl_primitive_product_mul!(Vector<V, T, N>, f64);
